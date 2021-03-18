0c40babaaca890e6e72be6fac6ed13adbb9cce4e
Edwin <edwintang10@gmail.com>
2021-03-19 01:34:32 +0800

    Revert "Inital Draft for Home Page"
    This reverts commit 0824c1bb0c7789d00436fb87b9d2461408e11a04.

--- src/Database Medical Monitor UI/assets/js/chart.js
+++ src/Database Medical Monitor UI/assets/js/chart.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	
 	// Bar Chart
+
 	var barChartData = {
 		labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
 		datasets: [{
@@ -9,6 +10,12 @@ $(document).ready(function() {
 			borderColor: 'rgba(0, 158, 251, 1)',
 			borderWidth: 1,
 			data: [35, 59, 80, 81, 56, 55, 40]
+		}, {
+			label: 'Dataset 2',
+			backgroundColor: 'rgba(255, 188, 53, 0.5)',
+			borderColor: 'rgba(255, 188, 53, 1)',
+			borderWidth: 1,
+			data: [28, 48, 40, 19, 86, 27, 90]
 		}]
 	};
 
@@ -32,6 +39,11 @@ $(document).ready(function() {
 			label: "My First dataset",
 			backgroundColor: "rgba(0, 158, 251, 0.5)",
 			data: [100, 70, 20, 100, 120, 50, 70, 50, 50, 100, 50, 90]
+		}, {
+		label: "My Second dataset",
+		backgroundColor: "rgba(255, 188, 53, 0.5)",
+		fill: true,
+		data: [28, 48, 40, 19, 86, 27, 20, 90, 50, 20, 90, 20]
 		}]
 	};
 	
@@ -51,28 +63,19 @@ $(document).ready(function() {
 		}
 	});
 	
-	var lineChartData = {
-		labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-		datasets: [{
-			label: "My First dataset",
-			backgroundColor: "rgba(0, 158, 251, 0.5)",
-			data: [100, 70, 20, 100, 120, 50, 70, 50, 50, 100, 50, 90]
-		}]
-	};
+	// Bar Chart 2
 	
-	var linectx = document.getElementById('linegraph').getContext('2d');
-	window.myLine = new Chart(linectx, {
-		type: 'line',
-		data: lineChartData,
-		options: {
-			responsive: true,
-			legend: {
-				display: false,
-			},
-			tooltips: {
-				mode: 'index',
-				intersect: false,
-			}
-		}
-	});
+    barChart();
+    
+    $(window).resize(function(){
+        barChart();
+    });
+    
+    function barChart(){
+        $('.bar-chart').find('.item-progress').each(function(){
+            var itemProgress = $(this),
+            itemProgressWidth = $(this).parent().width() * ($(this).data('percent') / 100);
+            itemProgress.css('width', itemProgressWidth);
+        });
+    };
 });
