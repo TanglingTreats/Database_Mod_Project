6a9d34ee9e9323507015f82fd810177994600169
FionaSeahShuHui98 <71055210+FionaSeahShuHui98@users.noreply.github.com>
2021-03-19 16:39:27 +0800

    #7-Frontend/HomePage

--- src/Database Medical Monitor UI/assets/js/chart.js
+++ src/Database Medical Monitor UI/assets/js/chart.js
@@ -1,53 +1,23 @@
 $(document).ready(function() {
-	
-	// Bar Chart
-
-	var barChartData = {
-		labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+	// Blood Pressure
+	var lineChartData = {
+		labels: ["0000","0100","0200","0300","0400","0500","0600","0700","0800","0900","1000","1100","1200","1300",
+		"1400","1500","1600","1700","1800","1900","2000","2100","2200","2300"],
 		datasets: [{
-			label: 'Dataset 1',
-			backgroundColor: 'rgba(0, 158, 251, 0.5)',
-			borderColor: 'rgba(0, 158, 251, 1)',
-			borderWidth: 1,
-			data: [35, 59, 80, 81, 56, 55, 40]
-		}, {
-			label: 'Dataset 2',
+			label: "Systolic Value",
+			backgroundColor: "rgba(0, 158, 251, 0.5)",
+			borderColor: 'rgba(0, 158, 251, 0.5)',
+			data: [90,75,83,99,101,120,121,131,135,107,65,72,87,98,105,113,117,127,138,77,91,100,68,108]
+		},
+		{
+			label: 'Distolic Value',
 			backgroundColor: 'rgba(255, 188, 53, 0.5)',
 			borderColor: 'rgba(255, 188, 53, 1)',
 			borderWidth: 1,
-			data: [28, 48, 40, 19, 86, 27, 90]
+			data: [35,37,36,29,43,35,49,54,57,60,61,63,67,71,72,45,79,80,82,26,85,89,20,55,25,27]
 		}]
 	};
-
-	var ctx = document.getElementById('bargraph').getContext('2d');
-	window.myBar = new Chart(ctx, {
-		type: 'bar',
-		data: barChartData,
-		options: {
-			responsive: true,
-			legend: {
-				display: false,
-			}
-		}
-	});
-
-	// Line Chart
-
-	var lineChartData = {
-		labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-		datasets: [{
-			label: "My First dataset",
-			backgroundColor: "rgba(0, 158, 251, 0.5)",
-			data: [100, 70, 20, 100, 120, 50, 70, 50, 50, 100, 50, 90]
-		}, {
-		label: "My Second dataset",
-		backgroundColor: "rgba(255, 188, 53, 0.5)",
-		fill: true,
-		data: [28, 48, 40, 19, 86, 27, 20, 90, 50, 20, 90, 20]
-		}]
-	};
-	
-	var linectx = document.getElementById('linegraph').getContext('2d');
+	var linectx = document.getElementById('Blood_Pressure').getContext('2d');
 	window.myLine = new Chart(linectx, {
 		type: 'line',
 		data: lineChartData,
@@ -63,19 +33,46 @@ $(document).ready(function() {
 		}
 	});
 	
-	// Bar Chart 2
+	// Temperature
+	var lineChartData = {
+		labels: ["0000","0100","0200","0300","0400","0500","0600","0700","0800","0900","1000","1100","1200","1300",
+		"1400","1500","1600","1700","1800","1900","2000","2100","2200","2300"],
+		datasets: [{
+			label: "Temperature",
+			backgroundColor: "rgba(0, 158, 251, 0.5)",
+			data: [36.1,35.3,35.4,35.5,36.9,36.0,36.1,36.2,36.3,36.4,36.4,36.7,36.4,36.5,36.5,37.1,37.5,36.4,36.7,36.8,36.1,36.8,37.8,37.0]
+		}]
+	};
 	
-    barChart();
-    
-    $(window).resize(function(){
-        barChart();
-    });
-    
-    function barChart(){
-        $('.bar-chart').find('.item-progress').each(function(){
-            var itemProgress = $(this),
-            itemProgressWidth = $(this).parent().width() * ($(this).data('percent') / 100);
-            itemProgress.css('width', itemProgressWidth);
-        });
-    };
+	var linectx = document.getElementById('Temperature').getContext('2d');
+	window.myLine = new Chart(linectx, {
+		type: 'line',
+		data: lineChartData,
+		options: {
+			responsive: true,
+			legend: {
+				display: false,
+			},
+			tooltips: {
+				mode: 'index',
+				intersect: false,
+			}
+		}
+	});
+	
+	var linectx = document.getElementById('Heart_Rate').getContext('2d');
+	window.myLine = new Chart(linectx, {
+		type: 'line',
+		data: lineChartData,
+		options: {
+			responsive: true,
+			legend: {
+				display: false,
+			},
+			tooltips: {
+				mode: 'index',
+				intersect: false,
+			}
+		}
+	});
 });
