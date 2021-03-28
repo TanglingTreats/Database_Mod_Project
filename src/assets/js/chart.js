edacf3f3b1010ad43313be3616383570442ec812
Edwin <edwintang10@gmail.com>
2021-03-28 21:26:31 +0800

    Add integration for backend data and frontend charts.

--- src/assets/js/chart.js
+++ src/assets/js/chart.js
@@ -13,6 +13,7 @@ $(document).ready(function() {
 			url: baseUrl + endpoint,
 			success: function(data) {
 				console.log(data);
+				generateCharts(data);
 			}
 		});
 	}
@@ -37,90 +38,120 @@ $(document).ready(function() {
 
 	getAllPatients(getFirstPatient);
 
-	// Blood Pressure
-	var lineChartData = {
-		labels: ["0000","0100","0200","0300","0400","0500","0600","0700","0800","0900","1000","1100","1200","1300",
-		"1400","1500","1600","1700","1800","1900","2000","2100","2200","2300"],
-		datasets: [{
-			label: "Heart Rate",
-			backgroundColor: "rgba(0, 158, 251, 0.5)",
-			borderColor: 'rgba(0, 158, 251, 0.5)',
-			data: [90,75,83,99,101,120,121,131,135,107,65,72,87,98,105,113,117,127,138,77,91,100,68,108]
-		}]
-	};
-	var linectx = document.getElementById('Heart_Rate').getContext('2d');
-	window.myLine = new Chart(linectx, {
-		type: 'line',
-		data: lineChartData,
-		options: {
-			responsive: true,
-			legend: {
-				display: false,
-			},
-			tooltips: {
-				mode: 'index',
-				intersect: false,
-			}
-		}
-	});
+	function generateCharts(patientData) {
 
-	var lineChartData = {
-		labels: ["0000","0100","0200","0300","0400","0500","0600","0700","0800","0900","1000","1100","1200","1300",
-		"1400","1500","1600","1700","1800","1900","2000","2100","2200","2300"],
-		datasets: [{
-			label: "Systolic Value",
-			backgroundColor: "rgba(0, 158, 251, 0.5)",
-			borderColor: 'rgba(0, 158, 251, 0.5)',
-			data: [90,75,83,99,101,120,121,131,135,107,65,72,87,98,105,113,117,127,138,77,91,100,68,108]
-		},
-		{
-			label: 'Distolic Value',
-			backgroundColor: 'rgba(255, 188, 53, 0.5)',
-			borderColor: 'rgba(255, 188, 53, 1)',
-			borderWidth: 1,
-			data: [35,37,36,29,43,35,49,54,57,60,61,63,67,71,72,45,79,80,82,26,85,89,20,55,25,27]
-		}]
-	};
-	var linectx = document.getElementById('Blood_Pressure').getContext('2d');
-	window.myLine = new Chart(linectx, {
-		type: 'line',
-		data: lineChartData,
-		options: {
-			responsive: true,
-			legend: {
-				display: false,
-			},
-			tooltips: {
-				mode: 'index',
-				intersect: false,
-			}
+		var heartRateData = [];
+		var systolicBp = [];
+		var diastolicBp = [];
+		var tempData = [];
+
+		for(i of patientData) {
+			heartRateData.push(i.heart_rate);
+			systolicBp.push(i.bp_systolic);
+			diastolicBp.push(i.bp_diastolic);
+			tempData.push(i.temperature);
 		}
-	});
-	
-	// Temperature
-	var lineChartData = {
-		labels: ["0000","0100","0200","0300","0400","0500","0600","0700","0800","0900","1000","1100","1200","1300",
-		"1400","1500","1600","1700","1800","1900","2000","2100","2200","2300"],
-		datasets: [{
-			label: "Temperature",
-			backgroundColor: "rgba(0, 158, 251, 0.5)",
-			data: [36.1,35.3,35.4,35.5,36.9,36.0,36.1,36.2,36.3,36.4,36.4,36.7,36.4,36.5,36.5,37.1,37.5,36.4,36.7,36.8,36.1,36.8,37.8,37.0]
-		}]
-	};
-	
-	var linectx = document.getElementById('Temperature').getContext('2d');
-	window.myLine = new Chart(linectx, {
-		type: 'line',
-		data: lineChartData,
-		options: {
-			responsive: true,
-			legend: {
-				display: false,
-			},
-			tooltips: {
-				mode: 'index',
-				intersect: false,
+
+		var bpData = {
+			"systolicBp": systolicBp,
+			"diastolicBp": diastolicBp
+		};
+
+		generateHeartRateChart(heartRateData);
+		generateBloodPressureChart(bpData);
+		generateTempChart(tempData);
+	}
+
+	function generateHeartRateChart(heartRateData) {
+		var lineChartData = {
+			labels: ["0000","0100","0200","0300","0400","0500","0600","0700","0800","0900","1000","1100","1200","1300",
+			"1400","1500","1600","1700","1800","1900","2000","2100","2200","2300"],
+			datasets: [{
+				label: "Heart Rate",
+				backgroundColor: "rgba(0, 158, 251, 0.5)",
+				borderColor: 'rgba(0, 158, 251, 0.5)',
+				data: heartRateData
+			}]
+		};
+		var linectx = document.getElementById('Heart_Rate').getContext('2d');
+		window.myLine = new Chart(linectx, {
+			type: 'line',
+			data: lineChartData,
+			options: {
+				responsive: true,
+				legend: {
+					display: false,
+				},
+				tooltips: {
+					mode: 'index',
+					intersect: false,
+				}
 			}
-		}
-	});
+		});
+	}
+	// Blood Pressure
+
+	function generateBloodPressureChart(bpData) {
+		var lineChartData = {
+			labels: ["0000","0100","0200","0300","0400","0500","0600","0700","0800","0900","1000","1100","1200","1300",
+			"1400","1500","1600","1700","1800","1900","2000","2100","2200","2300"],
+			datasets: [{
+				label: "Systolic Value",
+				backgroundColor: "rgba(0, 158, 251, 0.5)",
+				borderColor: 'rgba(0, 158, 251, 0.5)',
+				data: bpData.systolicBp
+			},
+			{
+				label: 'Diastolic Value',
+				backgroundColor: 'rgba(255, 188, 53, 0.5)',
+				borderColor: 'rgba(255, 188, 53, 1)',
+				borderWidth: 1,
+				data: bpData.diastolicBp
+			}]
+		};
+		var linectx = document.getElementById('Blood_Pressure').getContext('2d');
+		window.myLine = new Chart(linectx, {
+			type: 'line',
+			data: lineChartData,
+			options: {
+				responsive: true,
+				legend: {
+					display: false,
+				},
+				tooltips: {
+					mode: 'index',
+					intersect: false,
+				}
+			}
+		});
+	}
+	
+	function generateTempChart(tempData) {
+		// Temperature
+		var lineChartData = {
+			labels: ["0000","0100","0200","0300","0400","0500","0600","0700","0800","0900","1000","1100","1200","1300",
+			"1400","1500","1600","1700","1800","1900","2000","2100","2200","2300"],
+			datasets: [{
+				label: "Temperature",
+				backgroundColor: "rgba(0, 158, 251, 0.5)",
+				data: tempData
+			}]
+		};
+		
+		var linectx = document.getElementById('Temperature').getContext('2d');
+		window.myLine = new Chart(linectx, {
+			type: 'line',
+			data: lineChartData,
+			options: {
+				responsive: true,
+				legend: {
+					display: false,
+				},
+				tooltips: {
+					mode: 'index',
+					intersect: false,
+				}
+			}
+		});
+	}
 });
