d82fcf44627c7a67ec8485a8ea129c18115436bd
Edwin <edwintang10@gmail.com>
2021-04-04 17:44:26 +0800

    Fix timezone bug that causes SQL and MongoDb date retrieval to be different

--- models/db.sql.js
+++ models/db.sql.js
@@ -6,7 +6,8 @@ const connection = mysql.createConnection({
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
-  database: dbConfig.DB
+  database: dbConfig.DB,
+  timezone : "+00:00"
 });
 
 // connection.getConnection(error => {
