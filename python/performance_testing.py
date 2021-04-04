8e1649c29bb47e89d9743715ff0645654ea2ccf8
Edwin <edwintang10@gmail.com>
2021-04-04 19:01:08 +0800

    Refactor testing functions

--- python/performance_testing.py
+++ python/performance_testing.py
@@ -34,7 +34,11 @@ except:
     print("No hosts found! Did you start mongodb service?")
 
 # -------------- Test Params -----------------
-numOfRuns = 5
+if (len(sys.argv) > 1):
+    numOfRuns = int(sys.argv[1])
+else:
+    numOfRuns = 5
+    
 totalTimes = []
 # --------------------------------------------
 
@@ -45,36 +49,51 @@ def getAvgTimesInMilli(totalTimes):
     avgTime =  round((timeTaken / len(totalTimes)) * 1000, 2)
     return avgTime
 
+def printResults(type, sqlTime, noSqlTime):
+    if(sqlTime < noSqlTime):
+        print(f"\nSQL {type} time is {round((noSqlTime - sqlTime), 2)}ms faster than NoSQL on average")
+    else:
+        print(f"\nSQL {type} time is {round((sqlTime - noSqlTime), 2)}ms slower than NoSQL on average")
+        
+def runTest(queryCallback):
+    for i in range(numOfRuns):
+        startTime = time.time()
+        queryCallback()
+        endTime = time.time()
+        t = endTime - startTime
+        totalTimes.append(t)
+
 # Get cursor
 sql_cur = sql_conn.cursor(dictionary=True)
 # Get MongoDB instance
 db = mongo_client["csc2008_hospital"]
 
+
 patient_vital_collection = db["patient_vital"]
 print("\nTesting Query speeds for patient_vitals")
 
-for i in range(5):
-    startTime = time.time()
+def runSQLSelect():
     sql_cur.execute("SELECT * FROM patient_vitals")
-    res = sql_cur.fetchall()
-    endTime = time.time()
-    t = endTime - startTime
-    totalTimes.append(t)
+    sql_cur.fetchall()
+
+runTest(runSQLSelect)
     
+print("\nCalculating times for SQL")
 sqlAvgTime = getAvgTimesInMilli(totalTimes)
 print(f"Average time for SQL is: {sqlAvgTime}ms")
 
 totalTimes.clear()
     
-for i in range(5):
-    startTime = time.time()
+def runNoSQLFind():
     res = patient_vital_collection.find()
-    endTime = time.time()
-    t = endTime - startTime
-    totalTimes.append(t)
 
+runTest(runNoSQLFind)
+
+print("\nCalculating times for NoSQL")
 mongoAvgTime = getAvgTimesInMilli(totalTimes)
 print(f"Average time for NoSQL is: {mongoAvgTime}ms")
+
+printResults("Query", sqlAvgTime, mongoAvgTime)
 
 print("\nClosing connections")
 sql_conn.close()
