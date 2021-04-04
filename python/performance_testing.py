f475f07304b7a9bbac206548639c1a68fa4fd9f6
Edwin <edwintang10@gmail.com>
2021-04-04 18:26:44 +0800

    Add query performance testing

--- python/performance_testing.py
+++ python/performance_testing.py
@@ -33,10 +33,49 @@ try:
 except:
     print("No hosts found! Did you start mongodb service?")
 
+# -------------- Test Params -----------------
+numOfRuns = 5
+totalTimes = []
+# --------------------------------------------
+
+def getAvgTimesInMilli(totalTimes):
+    timeTaken = 0
+    for i in totalTimes:
+        timeTaken += i
+    avgTime =  round((timeTaken / len(totalTimes)) * 1000, 2)
+    return avgTime
 
 # Get cursor
 sql_cur = sql_conn.cursor(dictionary=True)
+# Get MongoDB instance
+db = mongo_client["csc2008_hospital"]
 
-print("Closing connections")
+patient_vital_collection = db["patient_vital"]
+print("\nTesting Query speeds for patient_vitals")
+
+for i in range(5):
+    startTime = time.time()
+    sql_cur.execute("SELECT * FROM patient_vitals")
+    res = sql_cur.fetchall()
+    endTime = time.time()
+    t = endTime - startTime
+    totalTimes.append(t)
+    
+sqlAvgTime = getAvgTimesInMilli(totalTimes)
+print(f"Average time for SQL is: {sqlAvgTime}ms")
+
+totalTimes.clear()
+    
+for i in range(5):
+    startTime = time.time()
+    res = patient_vital_collection.find()
+    endTime = time.time()
+    t = endTime - startTime
+    totalTimes.append(t)
+
+mongoAvgTime = getAvgTimesInMilli(totalTimes)
+print(f"Average time for NoSQL is: {mongoAvgTime}ms")
+
+print("\nClosing connections")
 sql_conn.close()
 mongo_client.close()
