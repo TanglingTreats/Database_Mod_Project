772163987c6d649c3b4bd5b2fef32fd48265c224
Edwin <edwintang10@gmail.com>
2021-04-05 22:40:18 +0800

    Change name of data-gen to data_gen

--- python/performance_testing.py
+++ python/performance_testing.py
@@ -3,6 +3,7 @@
 """
 
 from signal import signal, SIGINT
+import data_gen as DataGen
 import pprint
 import pandas
 import mariadb
@@ -39,6 +40,8 @@ if (len(sys.argv) > 1):
 else:
     numOfRuns = 5
     
+print(f"\nTest runs for {numOfRuns} times")
+    
 totalTimes = []
 
 tableName = "patient_vital"
@@ -65,19 +68,37 @@ def runTest(queryCallback):
         endTime = time.time()
         t = endTime - startTime
         totalTimes.append(t)
+        
+# ----------- SQL -------------
+def runSQLSelect():
+    sql_cur.execute(f"SELECT * FROM {tableName};")
+    return sql_cur.fetchall()
+
+def runSQLUpdate():
+    sql_cur.execute(f"UPDATE {tableName} SET heart_rate = 75;")
+    # Required to make changes to the database
+    sql_conn.commit()
+# ----------- NoSQL -------------
+def runNoSQLFind():
+    res = patient_vital_collection.find()
+    return res
+
+def runNoSQLUpdate():
+    patient_vital_collection.update_many({}, {'$set':{"heart_rate": 75}})
 
 # Get cursor
 sql_cur = sql_conn.cursor(dictionary=True)
 # Get MongoDB instance
 db = mongo_client["csc2008_hospital"]
 
-
 patient_vital_collection = db[f"{tableName}"]
-print(f"\nTesting Query speeds for {tableName}")
 
-def runSQLSelect():
-    sql_cur.execute(f"SELECT * FROM {tableName};")
-    sql_cur.fetchall()
+
+numOfEntries = len(runSQLSelect())
+print(f"Test is done on {numOfEntries} entries")
+
+# ---------- Query -----------
+print(f"\nTesting Query speeds for {tableName}")
 
 runTest(runSQLSelect)
     
@@ -86,9 +107,6 @@ sqlAvgTime = getAvgTimesInMilli(totalTimes)
 print(f"Average time for SQL is: {sqlAvgTime}ms")
 
     
-def runNoSQLFind():
-    res = patient_vital_collection.find()
-
 runTest(runNoSQLFind)
 
 print("\nCalculating times for NoSQL")
@@ -97,22 +115,20 @@ print(f"Average time for NoSQL is: {noSQLAvgTime}ms")
 
 printResults("Query", sqlAvgTime, noSQLAvgTime)
 
-print(f"\nTesting Update speeds for {tableName}")
-
-def runSQLUpdate():
-    sql_cur.execute(f"UPDATE {tableName} SET heart_rate = 75;")
-    # Required to make changes to the database
-    sql_conn.commit()
+# ---------- Update -----------
+print(f"\n\nTesting Update speeds for {tableName}")
 
 runTest(runSQLUpdate)
 
+print("\nCalculating times for SQL")
 sqlAvgTime = getAvgTimesInMilli(totalTimes)
 print(f"Average time for SQL is: {sqlAvgTime}ms")
 
-def runNoSQLUpdate():
-    patient_vital_collection.update_many({}, {'$set':{"heart_rate": 75}})
+
 
 runTest(runNoSQLUpdate)
+
+print("\nCalculating times for NoSQL")
 noSQLAvgTime = getAvgTimesInMilli(totalTimes)
 print(f"Average time for NoSQL is: {noSQLAvgTime}ms")
 
